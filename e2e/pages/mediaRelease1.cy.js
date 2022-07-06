fa798cd8cd2f8e577f68f924ae20f937a77ab5fa
lekshmyrm <105427818+lekshmyrm@users.noreply.github.com>
2022-07-07 00:54:53 +1000

    Update mediaRelease1.cy.js

--- e2e/pages/mediaRelease1.cy.js
+++ e2e/pages/mediaRelease1.cy.js
@@ -1,1 +1,32 @@
+/// <reference types="cypress" />
+import MediaPage from "../support/mediaPage"
+const mediapage = new MediaPage()
+describe('NSW Media Releases Page', () => {
+    before(() => {
+      cy.visit(Cypress.env('login_url')+'/media-releases')
+      })
+  
+    it('Select The Premier and Search', () => {
 
+      mediapage.selectOption('Filter by Minister')
+      mediapage.clickElement()
+      mediapage.clickButton('Search')
+          
+    })
+
+    it('Verify Grid is populated', () =>{
+      mediapage.gridVisible()
+      mediapage.shouldListCardItems()      
+    })
+
+    it('Reset link should be visible',() => {
+      mediapage.resetLinkVisible()  
+    })
+
+    it('Click Reset button',() =>{
+      mediapage.resetLinkClick()
+    })
+
+
+})
+  
